66240f6a1fbacd3ea02a3599ea11c341d83a26e7
Saeed shamsi <saeedshams2024@gmail.com>
2026-09-20 13:36:47 +0330

    Go: session-05 ? ???????? ??? ????? ? defer

---
+++ Go/session-05-functions-defer/examples/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("?????????? ??? ???")
+	}
+	return a / b, nil
+}
+func main() {
+	defer fmt.Println("????? ??????")
+	result, err := divide(10, 4)
+	if err != nil {
+		fmt.Println("???:", err)
+		return
+	}
+	fmt.Println("?????:", result)
+}
