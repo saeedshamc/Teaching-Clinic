26f0743e83b0e783edbe16889899eb9fc856519b
Saeed shamsi <saeedshams2024@gmail.com>
2026-09-20 13:45:25 +0330

    Go: session-08 — interfaces و polymorphism عملی

--- Go/session-08-interfaces/examples/main.go
+++ Go/session-08-interfaces/examples/main.go
@@ -2,13 +2,29 @@ package main
 
 import "fmt"
 
-type Notifier interface{ Send(message string) error }
+type Notifier interface {
+	Send(message string) error
+}
+
 type EmailNotifier struct{}
 
-func (EmailNotifier) Send(message string) error { fmt.Println("email:", message); return nil }
+func (EmailNotifier) Send(message string) error {
+	fmt.Println("email:", message)
+	return nil
+}
 
-type OrderService struct{ notifier Notifier }
+type OrderService struct {
+	notifier Notifier
+}
 
-func NewOrderService(n Notifier) *OrderService { return &OrderService{notifier: n} }
-func (s *OrderService) Place(id string) error  { return s.notifier.Send("order " + id + " ??? ??") }
-func main()                                    { _ = NewOrderService(EmailNotifier{}).Place("A-42") }
+func NewOrderService(n Notifier) *OrderService {
+	return &OrderService{notifier: n}
+}
+
+func (s *OrderService) Place(id string) error {
+	return s.notifier.Send("order " + id + " ثبت شد")
+}
+
+func main() {
+	_ = NewOrderService(EmailNotifier{}).Place("A-42")
+}
