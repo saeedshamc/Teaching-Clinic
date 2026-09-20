5b36722febd6a949bf547c5e96087f5f42edac68
Saeed shamsi <saeedshams2024@gmail.com>
2026-09-20 13:36:48 +0330

    Go: session-08 ? interface ? ????? polymorphic

---
+++ Go/session-08-interfaces/examples/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type Notifier interface{ Send(message string) error }
+type EmailNotifier struct{}
+
+func (EmailNotifier) Send(message string) error { fmt.Println("email:", message); return nil }
+
+type OrderService struct{ notifier Notifier }
+
+func NewOrderService(n Notifier) *OrderService { return &OrderService{notifier: n} }
+func (s *OrderService) Place(id string) error  { return s.notifier.Send("order " + id + " ??? ??") }
+func main()                                    { _ = NewOrderService(EmailNotifier{}).Place("A-42") }
